88ce7f3582c6ef09b886341a757e79209d24c73d
alshef <alsheffehsla@yandex.ru>
2024-04-10 01:38:45 +0300

    second commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+importScripts('js/sw-toolbox.js');
+
+toolbox.precache([
+  'styles/buttons.css',
+  'styles/forms.css',
+  'styles/inputs.css',
+  'styles/styles.css',
+  'styles/terminal.css',
+  'js/companion.js',
+  'js/forms.js',
+  'js/terminal.js',
+  'js/main.js',
+  'index.html',
+]);
